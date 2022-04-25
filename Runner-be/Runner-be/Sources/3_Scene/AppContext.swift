e6aa504f62d3828f34a2714cb1881dddf0edf15d
SHIVVVPP <sinsdhk@gmail.com>
2022-04-26 05:34:34 +0900

    App Context 추가

---
+++ Runner-be/Runner-be/Sources/3_Scene/AppContext.swift
@@ -0,0 +1,13 @@
+//
+//  AppContext.swift
+//  Runner-be
+//
+//  Created by 김신우 on 2022/04/26.
+//
+
+import UIKit
+
+class AppContext {
+    static let shared = AppContext()
+    private init() {}
+}
