5ddf280651f0c88f62b8e9bd4db8b7d5b4ed8049
SHIVVVPP <sinsdhk@gmail.com>
2022-10-11 23:40:43 +0900

    버전 변경

--- Runner-be/Runner-be/Sources/MainTabbar/MyPage/Settings/Config/SettingConfigItems.swift
+++ Runner-be/Runner-be/Sources/MainTabbar/MyPage/Settings/Config/SettingConfigItems.swift
@@ -76,7 +76,7 @@ extension PolicyCategory {
     var detail: String {
         switch self {
         case .version:
-            return "1.0.0"
+            return "1.2.0"
         default:
             return ""
         }
