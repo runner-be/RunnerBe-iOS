8800c9973302c5d4b90eac90656ee81fa3864100
SHIVVVPP <sinsdhk@gmail.com>
2022-10-21 17:44:18 +0900

    버전 -> 1.2.2

--- Runner-be/Runner-be/Sources/MainTabbar/MyPage/Settings/Config/SettingConfigItems.swift
+++ Runner-be/Runner-be/Sources/MainTabbar/MyPage/Settings/Config/SettingConfigItems.swift
@@ -76,7 +76,7 @@ extension PolicyCategory {
     var detail: String {
         switch self {
         case .version:
-            return "1.2.1"
+            return "1.2.2"
         default:
             return ""
         }
